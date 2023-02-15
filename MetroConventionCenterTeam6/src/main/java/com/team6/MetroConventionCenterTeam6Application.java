75ac974579aa7b0e0171a97c2c8942abffdf9c07
Stephen Lanciani <lanciani_stephen@bah.com>
2023-02-15 22:23:33 +0000

    day 1 code

---
+++ MetroConventionCenterTeam6/src/main/java/com/team6/MetroConventionCenterTeam6Application.java
@@ -0,0 +1,13 @@
+package com.team6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroConventionCenterTeam6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetroConventionCenterTeam6Application.class, args);
+	}
+
+}
---
+++ MetroConventionCenterTeam6/src/test/java/com/team6/conventioncenter/MetroConventionCenterTeam6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.conventioncenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroConventionCenterTeam6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
