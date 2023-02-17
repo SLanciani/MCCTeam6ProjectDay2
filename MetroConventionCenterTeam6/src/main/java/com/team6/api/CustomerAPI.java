b9dfff886f4a394b68dba435427ee883cf61f263
Morales-Kevin <Morales_Kevin@bah.com>
2023-02-17 20:23:27 +0000

    Adding customerApi calls

--- MetroConventionCenterTeam6/src/main/java/com/team6/api/CustomerAPI.java
+++ MetroConventionCenterTeam6/src/main/java/com/team6/api/CustomerAPI.java
@@ -1,55 +1,126 @@
 package com.team6.api;
 
+import java.net.URI;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Iterator;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.team6.domain.Customer;
+//import com.team6.logging.ApiLogger;
+import com.team6.repository.CustomersRepository;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerAPI {
-	ArrayList<Customer> customerList = new ArrayList<Customer>();
-
-	public CustomerAPI() {
-		Customer c1 = new Customer(1, "Austin", "pass", "austin@bah.com");
-		Customer c2 = new Customer(2, "Anne", "pass", "anne@bah.com");
-		Customer c3 = new Customer(3, "Timothy", "pass", "timothy@bah.com");
-		Customer c4 = new Customer(4, "Emily", "pass", "emily@bah.com");
-		Customer c5 = new Customer(5, "John", "pass", "john@bah.com");
-		Customer c6 = new Customer(6, "Morgan", "pass", "morgan@bah.com");
-		Customer c7 = new Customer(7, "Yerarmy", "pass", "Yerarmy@bah.com");
-		Customer c8 = new Customer(8, "Brian", "pass", "brian@bah.com");
-		
-		customerList.add(c1);
-		customerList.add(c2);
-		customerList.add(c3);
-		customerList.add(c4);
-		customerList.add(c5);
-		customerList.add(c6);
-		customerList.add(c7);
-		customerList.add(c8);
-	}
+	@Autowired
+	CustomersRepository repo;
 
 	@GetMapping
-	public Collection<Customer> getAll() {
-		return this.customerList;
+	public Iterable<Customer> getAll() {
+		return repo.findAll();
 	}
 
 	@GetMapping("/{customerId}")
-	public Customer getCustomerById(@PathVariable("customerId") long id) {
-		
-		Customer customer = null;
-		for (int i = 0; i < customerList.size(); i++) {
-			if (customerList.get(i).getId() == id) {
-				customer = customerList.get(i);
-			}
+	public Optional<Customer> getCustomerById(@PathVariable("customerId") long id) {
+		return repo.findById(id);
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> addCustomer(@RequestBody Customer newCustomer, UriComponentsBuilder uri) {
+		// Checks to see if name or email is null and that no id is passed
+		if(newCustomer.getId() != 0 || newCustomer.getName() == null || newCustomer.getEmail() == null) 
+		{
+			return ResponseEntity.badRequest().build();
 		}
-		return customer;
+	
+		//If everything clears save new customer to repo
+		newCustomer = repo.save(newCustomer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newCustomer.getId()).toUri();
+		ResponseEntity<?> response = ResponseEntity.created(location).build();
+		return response;
 	}
 
+	//lookupCustomerByName GET
+	@GetMapping("/byname/{username}")
+	public ResponseEntity<?> lookupCustomerByNameGet(@PathVariable("username") String username,
+			UriComponentsBuilder uri) {
+	
+		ArrayList<Customer> customers = new ArrayList<>();
+		repo.findAll().forEach(customers::add);
+		
+		customers.stream().filter(c -> c.getName().equals(username)).findAny().orElse(null);
+		
+		//URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("byname/{username}").buildAndExpand(customers.forEach(c -> c.getName())).toUri();
+		
+		
+		//ResponseEntity<?> response = ResponseEntity.ok().location(location).build();
+		return null;
+		//  Workshop:  Write an implemenatation to look up a customer by name.  Think about what
+		//  your response should be if no customer matches the name the caller is searching for.
+		//  With the data model implemented in CustomersRepository, do you need to handle more than
+		//  one match per request?
+	}
+	
+	//lookupCustomerByName POST
+	@PostMapping("/byname")
+	public ResponseEntity<?> lookupCustomerByNamePost(@RequestBody String username, UriComponentsBuilder uri) {
+		//  Workshop:  Write an implementation to look up a customer by name, using POST semantics
+		//  rather than GET.  You should be able to make use of most of your implmentation for
+		//  lookupCustomerByNameGet().  
+		return null;
+	}	
+	
+	
+	@PutMapping("/{customerId}")
+	public ResponseEntity<?> putCustomer(
+			@RequestBody Customer newCustomer,
+			@PathVariable("customerId") long customerId) 
+	{
+		// Ensure newCustomer name and email are not null and that the ids match
+		if(newCustomer.getId() != customerId || newCustomer.getName() == null || newCustomer.getEmail() == null)
+		{
+			return ResponseEntity.badRequest().build();
+		}
+		
+		//Update the customer's information
+		newCustomer = repo.save(newCustomer);
+		return ResponseEntity.ok().build();
+	}	
+	
+	@DeleteMapping("/{customerId}")
+	public ResponseEntity<?> deleteCustomerById(@PathVariable("customerId") long id) {
+		//Ensure that the id exists
+		if(repo.existsById(id) == false)
+		{
+			return ResponseEntity.badRequest().build();
+		}
+		
+		//If it exists delete the id
+		repo.deleteById(id);
+		return ResponseEntity.ok().build();
+		
+		
+		//  Implement a method to delete a customer.  What is an appropriate response? 
+		//
+		//  For discussion (do not worry about implementation):  What )are some ways of handling 
+		//  a "delete"?  Is it always the right thing from a business point of view to literally 
+		//  delete a customer entry?  If you did actually delete a customer entry, are there issues
+		//  you could potentially run into later? 
+	}	
+	
+	
 }
